f0a65c2070870ab9158abce1913e5e464abec421
Steven Culwell <beastwick18@gmail.com>
2022-09-16 03:42:28 -0500

    changed to ll

--- msh.c
+++ msh.c
@@ -24,68 +24,94 @@
 #define MAX_NUM_ARGUMENTS 11    // Mav shell supports up to 10 arguments, in
                                 // addition to the command
 
-typedef struct _queue queue;
-struct _queue {
-    size_t size;
-    char **data;
+typedef struct llist_node llist_node;
+struct llist_node {
+    struct llist_node *next;
+    struct llist_node *prev;
+    char *data;
 };
 
-// Create new queue
-queue *new_queue(size_t size) {
-    queue *q = (queue *) malloc(sizeof(queue));
-    q->data = (char **) calloc(size, sizeof(char *));
-    q->size = size;
-    return q;
+typedef struct llist llist;
+struct llist {
+    struct llist_node *head;
+    struct llist_node *tail;
+    int visible_length;
+};
+
+llist *new_llist(int visible_length) {
+    llist *ll = (llist *) calloc(1, sizeof(llist));
+    ll->visible_length = visible_length;
+    return ll;
 }
 
-// List each entry in queue labeled 1..n
-// Will stop once a NULL entry is found
-// because that is one past the last
-// entry in the queue
-void queue_list(queue *q) {
-    int idx = 1;
-    for(int i = q->size-1; i >= 0; i--) {
-        if(q->data[i] == NULL)
-            continue;
-        printf("%d: %s\n", idx++, q->data[i]);
+llist_node *new_llist_node(llist_node *prev, char *data) {
+    llist_node *node = (llist_node *) calloc(1, sizeof(llist_node));
+    node->prev = prev;
+    node->data = strndup(data, MAX_COMMAND_SIZE);
+    return node;
+}
+
+void llist_push_back(llist *ll, char *data) {
+    if(ll == NULL) {
+        return;
     }
-}
-
-void enqueue(queue *q, char *data) {
-    // Free last item on queue if it exists so
-    // that it can be overwritten in following
-    // for loop
-    if(q->data[q->size-1] != NULL)
-        free(q->data[q->size-1]);
-    // Shift all items in queue down to make space
-    // for new item at front of queue
-    for(size_t i = q->size-1; i >= 1; i--) {
-        q->data[i] = q->data[i-1];
+    if(ll->head == NULL) {
+        ll->head = new_llist_node(NULL, data);
+        ll->tail = ll->head;
+        return;
     }
-    if(q->data[0] != NULL)
-        free(q->data[0]);
-    q->data[0] = strndup(data, MAX_COMMAND_SIZE);
+    ll->tail->next = new_llist_node(ll->tail, data);
+    ll->tail = ll->tail->next;
 }
 
-char *queue_get(queue *q, int n) {
-    if(n < 1 || n >= q->size+1)
+llist_node *llist_go_back(llist_node *tail, int back) {
+    if(back == 0) {
+        return tail;
+    }
+    if(tail->prev == NULL) {
+        return tail;
+    }
+    return llist_go_back(tail->prev, back-1);
+}
+
+void llist_list_recursive(llist_node *node, int index) {
+    if(node == NULL)
+        return;
+    printf("%d: %s\n", index, node->data);
+    llist_list_recursive(node->next, index+1);
+}
+
+void llist_list(llist *ll) {
+    llist_node *starting_node = llist_go_back(ll->tail, ll->visible_length-1);
+    llist_list_recursive(starting_node, 1);
+}
+
+char *llist_get_recursive(llist_node *node, int index) {
+    if(node == NULL)
         return NULL;
-    for(size_t i = q->size-1; i >= 0; i--) {
-        if(q->data[i] == NULL && --n == 0) {
-            return q->data[i];
-        }
-    }
-    return NULL;
+    if(index == 0)
+        return node->data;
+    return llist_get_recursive(node->next, index-1);
 }
 
-// Free all items in queue, as well as the queue itself
-void queue_free(queue *q) {
-    for(size_t i = 0; i < q->size; i++) {
-        if(q->data[i])
-            free(q->data[i]);
-    }
-    free(q->data);
-    free(q);
+char *llist_get(llist *ll, int index) {
+    if(index < 1 || index > ll->visible_length)
+        return NULL;
+    llist_node *starting_node = llist_go_back(ll->tail, ll->visible_length-1);
+    return llist_get_recursive(starting_node, index-1);
+}
+
+void llist_free_recursive(llist_node *head) {
+    if(head == NULL)
+        return;
+    llist_free_recursive(head->next);
+    free(head->data);
+    free(head);
+}
+
+void llist_free(llist *ll) {
+    llist_free_recursive(ll->head);
+    free(ll);
 }
 
 bool file_exists(char *file_name) {
@@ -94,11 +120,9 @@ bool file_exists(char *file_name) {
 
 bool is_number(char *str) {
     int len = strnlen(str, MAX_COMMAND_SIZE);
-    for(int i = 0; i < len; i++) {
-        if(!isdigit(str[i])) {
+    for(int i = 0; i < len; i++)
+        if(!isdigit(str[i]))
             return false;
-        }
-    }
     return true;
 }
 
@@ -127,12 +151,25 @@ bool find_command(char *command, char *command_location) {
            find_command_in_path("/bin/", command, command_location);
 }
 
+pid_t spawn_process(char *command, char **args) {
+    pid_t child = fork();
+    if (child == 0) {
+        execvp(command, args);
+        perror(args[0]);
+        exit(EXIT_FAILURE);
+    } else {
+        int status = 0;
+        waitpid(child, &status, 0);
+        return child;
+    }
+}
+
 int main() {
     char *command_string = (char *) malloc(MAX_COMMAND_SIZE);
     char *command_location = (char *) malloc(MAX_COMMAND_SIZE);
     char *full_command = (char *) malloc(MAX_COMMAND_SIZE);
-    queue *pids = new_queue(20);
-    queue *history = new_queue(15);
+    llist *pids = new_llist(20);
+    llist *history = new_llist(15);
     
     while(true) {
         // Print out the msh prompt
@@ -151,18 +188,19 @@ int main() {
         // Check if user entered "!n", where n is the index of a command in history.
         // If they did, we will replace the current command with the nth command in
         // history so that it can be tokenized
-        if(strnlen(command_string, MAX_COMMAND_SIZE) > 1 &&
-                command_string[0] == '!' &&
-                is_number(&command_string[1])) {
+        if(strnlen(full_command, MAX_COMMAND_SIZE) > 1 &&
+                full_command[0] == '!' &&
+                is_number(&full_command[1])) {
                 
-            int idx = atoi(&command_string[1]);
-            char *new_command_string = queue_get(history, idx);
+            int idx = atoi(&full_command[1]);
+            char *new_command_string = llist_get(history, idx);
             if(new_command_string == NULL) {
                 fprintf(stderr, "Command not in history.\n");
                 continue;
             }
             // printf("%s\n", new_command_string);
             strncpy(command_string, new_command_string, MAX_COMMAND_SIZE);
+            strncpy(full_command, new_command_string, MAX_COMMAND_SIZE);
         }
         
         /* Parse input */
@@ -176,6 +214,7 @@ int main() {
 
         char *working_string = strdup( command_string );                
         
+        // printf(" # %d %d", history->head != NULL, history->tail != NULL);
 
         // we are going to move the working_string pointer so
         // keep track of its original value so we can deallocate
@@ -202,7 +241,7 @@ int main() {
         }
         
         // Add current command to history
-        // enqueue(history, full_command);
+        llist_push_back(history, full_command);
         
         // The shell will exit with status 0 if the command "quit" or "exit" is given
         if(strncmp(token[0], "exit", MAX_COMMAND_SIZE) == 0 ||
@@ -224,22 +263,15 @@ int main() {
                 }
             }
         } else if(strncmp(token[0], "listpids", MAX_COMMAND_SIZE) == 0) {
-            queue_list(pids);
+            llist_list(pids);
         } else if(strncmp(token[0], "history", MAX_COMMAND_SIZE) == 0) {
-            queue_list(history);
+            llist_list(history);
         } else if(find_command(token[0], command_location)) {
-            pid_t child = fork();
-            if (child == 0) {
-                execvp(command_location, &token[0]);
-                perror(token[0]);
-                exit(EXIT_FAILURE);
-            } else {
-                int status = 0;
-                char buffer[MAX_COMMAND_SIZE] = { 0 };
-                sprintf(buffer, "%d", child);
-                enqueue(pids, buffer);
-                waitpid(child, &status, 0);
-            }
+            pid_t child = spawn_process(command_location, &token[0]);
+            
+            char buffer[MAX_COMMAND_SIZE] = { 0 };
+            snprintf(buffer, MAX_COMMAND_SIZE, "%d", child);
+            llist_push_back(pids, buffer);
         }  else {
             printf("%s: Command not found.\n", token[0]);
         }
@@ -248,8 +280,8 @@ int main() {
             free(token[i]);
         }
     }
-    queue_free(pids);
-    queue_free(history);
+    llist_free(pids);
+    llist_free(history);
     free(command_string);
     free(command_location);
     free(full_command);
